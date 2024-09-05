e42f7a67e0e2a1e6c5adf7dc73adf77f49a97a55
Scott Robinson <scott.robinson4477@gmail.com>
2024-09-05 13:01:57 +0100

    now uses computed properties

--- index.js
+++ index.js
@@ -79,29 +79,34 @@ function ref(raw) {
   return r
 }
 
-let product = reactive({ price: 5, quantity: 2 })
-let salePrice = ref(0)
-let total = 0
+function computed(getter) {
+  let result = ref()
 
-effect(() => {
-  salePrice.value = product.price * 0.9
+  effect(() => (result.value = getter()))
+
+  return result
+}
+
+let product = reactive({ price: 5, quantity: 2 })
+let salePrice = computed(() => {
+  return product.price * 0.9
 })
-effect(() => {
-  total = salePrice.value * product.quantity
+let total = computed(() => {
+  return salePrice.value * product.quantity
 })
 
 console.log(
-  `Before updated total (should be 9) = ${total} salePrice (should be 4.5) = ${salePrice.value}`
+  `Before updated total (should be 9) = ${total.value} salePrice (should be 4.5) = ${salePrice.value}`
 )
 
 product.quantity = 3
 
 console.log(
-  `After updated total (should be 13.5) = ${total} salePrice (should be 4.5) = ${salePrice.value}`
+  `After updated total (should be 13.5) = ${total.value} salePrice (should be 4.5) = ${salePrice.value}`
 )
 
 product.price = 10
 
 console.log(
-  `After updated total (should be 27) = ${total} salePrice (should be 9) = ${salePrice.value}`
+  `After updated total (should be 27) = ${total.value} salePrice (should be 9) = ${salePrice.value}`
 )
